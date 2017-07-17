bb0d322ea2e6607c024195407e1aa14943823ec1
Cristian Valero Abundio <pauito03@hotmail.com>
2017-07-17 04:24:28 +0200

    New updates
    Signed-off-by: Cristian Valero Abundio <pauito03@hotmail.com>

--- src/com/cristianvalero/chat/servidor/com/cristianvalero/chat/servidor/utils/LogType.java
+++ src/com/cristianvalero/chat/servidor/com/cristianvalero/chat/servidor/utils/LogType.java
@@ -2,6 +2,7 @@ package com.cristianvalero.chat.servidor.com.cristianvalero.chat.servidor.utils;
 
 public enum LogType
 {
+<<<<<<< HEAD
     NORMALLY ("[]"),
     WARNING ("[WARNING]"),
     ERROR ("[ALERT/SEVERE]");
@@ -11,10 +12,31 @@ public enum LogType
     LogType(String prefix)
     {
         this.prefix = prefix;
+=======
+    NORMALLY ("[]", 0),
+    WARNING ("[WARNING]", 50),
+    ERROR ("[ALERT/SEVERE]", 100);
+
+    private String prefix;
+    private int value;
+
+    LogType(String prefix, int value)
+    {
+        this.prefix = prefix;
+        this.value = value;
+>>>>>>> 18e07c3... New updates
     }
 
     public String getPrefix()
     {
         return this.prefix;
     }
+<<<<<<< HEAD
+=======
+
+    public int getValue()
+    {
+        return this.value;
+    }
+>>>>>>> 18e07c3... New updates
 }
