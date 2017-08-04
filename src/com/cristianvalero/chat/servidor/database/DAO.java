3b0f7f3a5e181cc96c8056725ff38ce2601ef12f
Cristian Valero Abundio <pauito03@hotmail.com>
2017-08-04 19:43:59 +0200

    Facade and implemented new methods on DAO class ;)
    Signed-off-by: Cristian Valero Abundio <pauito03@hotmail.com>

--- src/com/cristianvalero/chat/servidor/database/DAO.java
+++ src/com/cristianvalero/chat/servidor/database/DAO.java
@@ -3,6 +3,7 @@ package com.cristianvalero.chat.servidor.database;
 import com.cristianvalero.chat.servidor.ejcServer;
 import com.cristianvalero.chat.servidor.utils.ClientRank;
 import com.cristianvalero.chat.servidor.utils.LogType;
+import com.sun.istack.internal.NotNull;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,7 +13,7 @@ import java.util.Vector;
 
 public class DAO //Data access object
 {
-    public static Collection<ClientData> getAllUsersDatabase()
+    protected static Collection<ClientData> getAllUsersDatabase()
     {
         Connection con = Database.getConnection(ejcServer.getNormallyDatabaseName());
         Vector<ClientData> v = new Vector<>();
@@ -45,5 +46,76 @@ public class DAO //Data access object
             ejcServer.log(" - "+e.getMessage(), LogType.MYSQL_ERROR);
         }
         return v;
+    }
+
+    protected static boolean equalsPassword(@NotNull String email, @NotNull String pass)
+    {
+        boolean a = false;
+        Connection con = Database.getConnection(ejcServer.getNormallyDatabaseName());
+
+        final String queryPasswd = "SELECT passwd FROM usuarios WHERE email = '"+email+"';";
+
+        try
+        {
+            ResultSet resQueryPasswd = con.prepareStatement(queryPasswd).executeQuery();
+            if (resQueryPasswd.next())
+                if (resQueryPasswd.getString("passwd").equals(pass))
+                    a = true;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            ejcServer.log("Ha ocurrido un error obteniendo datos de usuarios en la base de datos '"+ejcServer.getNormallyDatabaseName()+"'.", LogType.MYSQL_ERROR);
+            ejcServer.log(" - "+e.getMessage(), LogType.MYSQL_ERROR);
+        }
+        return a;
+    }
+
+    protected static boolean checkNickname(@NotNull String name)
+    {
+        boolean a = false;
+        Connection con = Database.getConnection(ejcServer.getNormallyDatabaseName());
+
+        final String queryNick = "SELECT name FROM usuarios;";
+
+        try
+        {
+            ResultSet resQueryNick = con.prepareStatement(queryNick).executeQuery();
+            while (resQueryNick.next())
+                if (resQueryNick.getString("name").equals(name))
+                    a = true;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            ejcServer.log("Ha ocurrido un error obteniendo datos de usuarios en la base de datos '"+ejcServer.getNormallyDatabaseName()+"'.", LogType.MYSQL_ERROR);
+            ejcServer.log(" - "+e.getMessage(), LogType.MYSQL_ERROR);
+        }
+
+        return a;
+    }
+
+    protected static boolean checkEmail(@NotNull String email)
+    {
+        boolean a = false;
+        Connection con = Database.getConnection(ejcServer.getNormallyDatabaseName());
+
+        final String queryEmail = "SELECT email FROM usuarios;";
+
+        try
+        {
+            ResultSet resQueryEmail = con.prepareStatement(queryEmail).executeQuery();
+            while (resQueryEmail.next())
+                if (resQueryEmail.getString("email").equals(email))
+                    a = true;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            ejcServer.log("Ha ocurrido un error obteniendo datos de usuarios en la base de datos '"+ejcServer.getNormallyDatabaseName()+"'.", LogType.MYSQL_ERROR);
+            ejcServer.log(" - "+e.getMessage(), LogType.MYSQL_ERROR);
+        }
+
+        return a;
     }
 }
--- src/com/cristianvalero/chat/servidor/database/Facade.java
+++ src/com/cristianvalero/chat/servidor/database/Facade.java
@@ -2,12 +2,32 @@ package com.cristianvalero.chat.servidor.database;
 import com.sun.istack.internal.NotNull;
 import org.apache.commons.codec.digest.DigestUtils;
 
-public class Facade
+import java.util.Collection;
+
+public class Facade extends DAO
 {
     public static String encryptPassword(@NotNull String normalPassword) //Your password is safe ;)
     {
         return DigestUtils.sha1Hex(DigestUtils.md5Hex(normalPassword));
     }
 
+    public static Collection<ClientData> getAllUsers() //Get temp Collection of ClientDats from Database
+    {
+        return Facade.getAllUsersDatabase();
+    }
 
+    public static boolean equalsPasswd(String email, String pass) //Check if X password is correct from X user
+    {
+        return Facade.equalsPassword(email, pass);
+    }
+
+    public static boolean nicknameExists(String nick)
+    {
+        return Facade.checkNickname(nick);
+    }
+
+    public static boolean emailExists(String email)
+    {
+        return Facade.checkEmail(email);
+    }
 }
